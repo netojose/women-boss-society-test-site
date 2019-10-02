f48b8e64aa105814900ec4fe58e4825aa770f896
José Neto <sputinykster@gmail.com>
2019-10-02 14:19:51 -0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,4 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
+export default () => <p css={css`color:red`}>Women Boss Society APP</p>
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devtool: 'cheap-module-eavl-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'build')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })
+  ]
+};
